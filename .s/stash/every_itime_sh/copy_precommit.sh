3d428f975e1506d658c4fd6fd32fd07bab5bf6f0
[alt]legioner9 <legioner9@inbox.ru>
2023-04-10 14:39:17 +0700

    <> #3

--- .s/stash/every_itime_sh/copy_precommit.sh
+++ .s/stash/every_itime_sh/copy_precommit.sh
@@ -2,7 +2,7 @@
 
 #. "$HOME/.bashrc"
 
-filename="${FONSH_PATH}/stash/every_itime_sh/copy_precommit.sh" 
+filename="${PLT_PATH}/.s/stash/every_itime_sh/copy_precommit.sh" 
 
 echo -e "${HLIGHT}---start file://$filename ---${NORMAL}" # start file
 
@@ -13,7 +13,7 @@ cd "$(prs_f -d $filename)" || qq_exit "$(prs_f -d $filename) not found"
 
 # garg_ $(prs_f -n $filename) $@ 1>/dev/null
 
-wrp_fifs2_ cp -uf ${FONSH_PATH}/stash/pre-commit ${FONSH_PATH}/.git/hooks -f1 -d2
+wrp2_ cp -uf --_xxf ${PLT_PATH}/.s/stash/pre-commit ${PLT_PATH}/.git/hooks
 
 cd "$idir"
 
