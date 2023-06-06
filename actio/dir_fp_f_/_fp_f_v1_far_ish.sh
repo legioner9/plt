bb7cbdb000ad9a61d3e427697705eae0e515b78a
[alt]legioner9 <legioner9@inbox.ru>
2023-06-06 15:37:01 +0700

    <> upfix ish fp_f_ #2

--- actio/dir_fp_f_/_fp_f_v1_far_ish.sh
+++ actio/dir_fp_f_/_fp_f_v1_far_ish.sh
@@ -308,34 +308,33 @@ ${NORMAL}"
     # -----------------------------------------------------------------------------------------
     # ------------------------------------------
 
-    # #? ARGS : name_fn_ dir_fns_flow_
+    #? ARGS : name_fn_ dir_fns_flow_
 
-    # if typeset -F | grep -w ${name_fn_}; then
-    #     plt_exit " not unique --_name_fn: ${name_fn_} "
-    #     return 1
-    # fi
+    if typeset -F | grep -w ${name_fn_}; then
+        plt_exit " not unique --_name_fn: ${name_fn_} "
+        return 1
+    fi
 
-    # if [ -d ${dir_fns_} ]; then
-    #     cd ${dir_fns_}
-    # else
-    #     plt_exit " not exist dir --_dir_fns: ${dir_fns_} "
-    #     return 1
-    # fi
+    if [ -d ${dir_fns_} ]; then
+        cd ${dir_fns_}
+    else
+        plt_exit " not exist dir --_dir_fns: ${dir_fns_} "
+        return 1
+    fi
 
-    # if ! is_num ${flow_}; then
-    #     plt_exit " not a number --_dir_fns: ${flow_} "
-    #     return 1
-    # fi
+    if ! is_num ${flow_}; then
+        plt_exit " not a number --_dir_fns: ${flow_} "
+        return 1
+    fi
 
-    # exl_ --list ${PLT_PATH}/.d/.mul/fo_f_/env/flow_env.exl
+    exl_ --list ${PLT_PATH}/.d/.mul/fo_f_/env/flow_env.exl
 
-    # plt_pause "(${name_fn_}) create flow=${flow_} !!! in ($PWD/dir_${name_fn_}/) ?"
+    plt_pause "(${name_fn_}) create flow=${flow_} !!! in ($PWD/dir_${name_fn_}/) ?"
 
-    # # exl_ --list "${exl_file}"
-    # # echo | . ${sh_file}
+    # exl_ --list "${exl_file}"
+    # echo | . ${sh_file}
 
-    # . ${sh_file}
-    . ${FN_DIR}/dir_${FNN}/${FNN}.ish
+    . ${sh_file}
 
     # ------------------------------------------
     # -----------------------------------------------------------------------------------------
