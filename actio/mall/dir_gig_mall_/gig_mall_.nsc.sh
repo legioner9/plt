b9aa4770f3bebb030d4f199be9e798357938042d
[alt]legioner9 <legioner9@inbox.ru>
2023-07-25 16:40:30 +0700

    <> do gig_mall as fp_f_ #2

---
+++ actio/mall/dir_gig_mall_/gig_mall_.nsc.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#! debag
+# . "${HOME}/.bashrc"
+#!
+
+filename="${PLT_PATH}/actio/mall/dir_gig_mall_/gig_mall_.sh"
+
+echo -e "${HLIGHT}--- start file: $filename with args: $@ ---${NORMAL}" # start file
+
+#e_exer "Questions? See far!" "$filename" "$LINENO"
+
+#up_lib_git
+
+gig_mall_() {
+
+    #{def_const}
+
+    #{ctrl_args}
+
+local FNN=${FUNCNAME[0]}
+# echo ". ${PLT_PATH}/.d/.nid/mall.nid"
+if ! . "${PLT_PATH}/.d/.nid/mall.nid"; then
+${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall.nid"
+return 1
+fi
+if [[ -n "${ARGS1}" ]] && [[ "${cntl_args[@]}" =~ "${ARGS1}" ]]; then
+return 0
+fi
+# echo ". ${FN_SAL_DIR}/${FNN}.body.nsc.sh"
+if ! . "${FN_SAL_DIR}/${FNN}.body.nsc.sh"; then
+${_plt_exit} "fail: . ${FN_SAL_DIR}/${FNN}.body.sh"
+return 1
+fi
+cd "$PPWD" 1>/dev/null || plt_exit "fail : cd $PPWD"
+    #{init_body}
+
+}
+
+# gig_mall_ @
+
+unset filename
