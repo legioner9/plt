673ea54d2cafbe084ca5dc4e00c8510d877f9bc3
[alt]legioner9 <legioner9@inbox.ru>
2023-07-27 02:22:36 +0700

    <> do prim #2

--- .d/.mul/fp_f_/prim/prov/.tml/file.body.sh
+++ .d/.mul/fp_f_/prim/prov/.tml/file.body.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo -e "${GREEN}\${g_args[@]} = ${g_args[*]}${NORMAL}" #print variable
-echo -e "${GREEN}\${e_args[@]} = ${e_args[*]}${NORMAL}" #print variable
-echo -e "${GREEN}\${f_args[@]} = ${f_args[*]}${NORMAL}" #print variable
 echo -e "${GREEN}\${ARGS[@]} = ${ARGS[*]}${NORMAL}" #print variable
 
 echo "that {name_fn_}.sal"
---
+++ actio/dir_fp_f_/prim_extst/extst_1/ex1/_deb/res_finis_1/dir_ln2_/ln2_.nsc.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#! debag
+# . "${HOME}/.bashrc"
+#!
+
+filename="${PLT_PATH}/actio/dir_fp_f_/prim_extst/extst_1/ex1/_deb/res/dir_ln2_/ln2_.sh"
+
+echo -e "${HLIGHT}--- start file: $filename with args: $@ ---${NORMAL}" # start file
+
+#e_exer "Questions? See far!" "$filename" "$LINENO"
+
+#up_lib_git
+
+ln2_() {
+
+dir_fns_="${PLT_PATH}/actio/dir_fp_f_/prim_extst/extst_1/ex1/_deb/res"
+    #{def_const}
+
+    #{ctrl_args}
+
+local FNN=${FUNCNAME[0]}
+# echo ". ${PLT_PATH}/.d/.nid/mall.nid"
+if ! . "${PLT_PATH}/.d/.nid/mall.nid"; then
+echo -e "${RED}--- fail: . ${PLT_PATH}/.d/.nid/mall.nid ---${NORMAL}" #exit 1
+return 1
+fi
+if [[ -n "${ARGS1}" ]] && [[ "${cntl_args[@]}" =~ "${ARGS1}" ]]; then
+return 0
+fi
+# echo ". ${FN_SAL_DIR}/${FNN}.body.nsc.sh"
+if ! . "${FN_SAL_DIR}/${FNN}.body.nsc.sh"; then
+echo -e "${RED}--- fail: . ${FN_SAL_DIR}/${FNN}.body.sh ---${NORMAL}"
+return 1
+fi
+cd "$PPWD" 1>/dev/null || echo -e "${RED}--- fail : cd $PPWD ---${NORMAL}"
+    #{init_body}
+
+}
+
+# ln2_ @
+
+unset filename
