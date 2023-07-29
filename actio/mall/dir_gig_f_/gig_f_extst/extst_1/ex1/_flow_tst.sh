f07e1018a94d73f5dbea965a87b0ad3705b2215d
[alt]legioner9 <legioner9@inbox.ru>
2023-07-29 08:14:19 +0700

    <> done gig_f_ v1 #2

--- actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_flow_tst.sh
+++ actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_flow_tst.sh
@@ -10,9 +10,19 @@ cd "${idir}" || {
 #?----------------------------------------------------
 #?-------------------------------------
 
-: >res
+unset lg1_
+# gig_f_extst/extst_1/ex1
+${_wrp2_} mkdir -p --_cnd ${PLT_PATH}/actio/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/
+${_wrp2_} mkdir -p --_cnd ${PLT_PATH}/actio/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/pre/
 
-# cr_tst_dir_ <dat &>>res
+${_wrp2_} rm -rfv --_cxd ${PLT_PATH}/actio/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_
+
+${_wrp2_} rm -fv ${PATH_SAL_AX_DIR}/lg1_.sal
+
+#? --_grad (1_prim|2_secu|3_mall|4_raqu|5_ague) --_tail (1_veri|2_prov|3_absc|...)
+
+# echo | fo_f_ --_name_fn nnn2_ --_dir_fns ${PLT_PATH}/actio/dir_gig_f_/fo_f_1_extst/extst_1/ex1/_deb/res/ --_flow 1
+echo | ${_fp_f_} --_name_fn lg1_ --_dir_fns ${PLT_PATH}/actio/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res --_grad mall --_tail prov
 
 #{body_sh}
 #?-------------------------------------
--- actio/mall/dir_gig_mall_/.d.ax/.sal.ax/gig_mall_.body.nsc.sh
+++
@@ -1,1 +0,0 @@
-/home/st/REPOBARE/_repo/plt/.d/.sal.ax/gig_mall_.sal
