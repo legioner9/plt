f88198920ce59174da90bd92011a4f7608533d22
[alt]legioner9 <legioner9@inbox.ru>
2023-08-15 16:21:34 +0700

    <> plt_is_ques del #2

--- .d/.mul/gig_f_/mall/prov/.exl/main.d.oll/08_cr_ln_s.sh
+++ .d/.mul/gig_f_/mall/prov/.exl/main.d.oll/08_cr_ln_s.sh
@@ -19,7 +19,7 @@ read -p "----------continue with that pram?---------------"
 
 if [ -f "${fn_body_ln}" ]; then
     if plt_is_ques "delit file ${fn_body_ln}?"; then
-
+echo 1
         "${_ehh}" rm -v ${fn_body_ln}
 
         "${_ehh}" cp -rfv "${tml_dir}/file.body.sh" "${fn_body_ln}"
@@ -27,6 +27,7 @@ if [ -f "${fn_body_ln}" ]; then
         "${_ehh}" "${_cr_f_}" "${fn_body_ln}" --vi0 ${name_fn_} --vr0 {name_fn_}
     fi
 else
+echo 2
     "${_ehh}" cp -rfv "${tml_dir}/file.body.sh" "${fn_body_ln}"
 
     "${_ehh}" "${_cr_f_}" "${fn_body_ln}" --vi0 ${name_fn_} --vr0 {name_fn_}
@@ -49,7 +50,7 @@ read -p "----------continue with that pram?---------------"
 
 if [ -f "${fn_body_ln}" ]; then
     if plt_is_ques "delit file ${fn_body_ln}?"; then
-
+echo 3
         "${_ehh}" rm -v ${fn_body_ln}
 
         "${_ehh}" cp -rfv "${tml_dir}/file.local.list" "${fn_body_ln}"
@@ -57,6 +58,7 @@ if [ -f "${fn_body_ln}" ]; then
         "${_ehh}" "${_cr_f_}" "${fn_body_ln}" --vi0 ${name_fn_} --vr0 {name_fn_}
     fi
 else
+echo 4
     "${_ehh}" cp -rfv "${tml_dir}/file.local.list" "${fn_body_ln}"
 
     "${_ehh}" "${_cr_f_}" "${fn_body_ln}" --vi0 ${name_fn_} --vr0 {name_fn_}
@@ -69,25 +71,29 @@ fi
 
 #? file.local.list -> file.h.sh
 
+fn_h_tml=${PATH_NID_DIR}/${grad_}/${grad_}.h
+echo -e "${GREEN}\$fn_h_tml = $fn_h_tml${NORMAL}" #print variable
+
 fn_body_ln=${PATH_SAL_AX_DIR}/${name_fn_}/${name_fn_}.h
 echo -e "${GREEN}\$fn_body_ln = $fn_body_ln${NORMAL}" #print variable
 
 fn_body_list=${_dotd_dir_sal}/${name_fn_}.h.sh
-echo -e "${GREEN}\$fn_body_sh = $fn_body_sh${NORMAL}" #print variable
+echo -e "${GREEN}\$fn_body_list = $fn_body_list${NORMAL}" #print variable
 
 read -p "----------continue with that pram?---------------"
 
 if [ -f "${fn_body_ln}" ]; then
     if plt_is_ques "delit file ${fn_body_ln}?"; then
-
+echo 5
         "${_ehh}" rm -v ${fn_body_ln}
 
-        "${_ehh}" cp -rfv "${tml_dir}/file.h.sh" "${fn_body_ln}"
+        "${_ehh}" cp -rfv "${fn_h_tml}" "${fn_body_ln}"
 
         "${_ehh}" "${_cr_f_}" "${fn_body_ln}" --vi0 ${name_fn_} --vr0 {name_fn_}
     fi
 else
-    "${_ehh}" cp -rfv "${tml_dir}/file.h.sh" "${fn_body_ln}"
+echo 6
+    "${_ehh}" cp -rfv "${fn_h_tml}" "${fn_body_ln}"
 
     "${_ehh}" "${_cr_f_}" "${fn_body_ln}" --vi0 ${name_fn_} --vr0 {name_fn_}
 
@@ -96,3 +102,39 @@ fi
 if ! "${_ehh}" ln -sv "${fn_body_ln}" "${fn_body_list}"; then
     "${_plt_exit}" "ln -sv ${fn_body_ln} ${fn_body_list}"
 fi
+
+
+#? dir h
+
+fn_dir_h_tml=${PATH_NID_DIR}/${grad_}/${grad_}.d.h
+echo -e "${GREEN}\$fn_dir_h_tml = $fn_dir_h_tml${NORMAL}" #print variable
+
+fn_dir_h_ln=${PATH_SAL_AX_DIR}/${name_fn_}/${name_fn_}.d.h
+echo -e "${GREEN}\$fn_dir_h_ln = $fn_dir_h_ln${NORMAL}" #print variable
+
+fn_dir_h_in=${_dotd_dir_sal}/${name_fn_}.d.h
+echo -e "${GREEN}\$fn_dir_h_in = $fn_dir_h_in${NORMAL}" #print variable
+
+read -p "----------continue with that pram?---------------"
+
+if [ -d "${fn_dir_h_ln}" ]; then
+    if plt_is_ques "delit dir ${fn_dir_h_ln}?"; then
+echo 7
+        "${_ehh}" rm -v ${fn_dir_h_ln}
+
+        "${_ehh}" cp -rfv "${fn_dir_h_tml}" "${fn_dir_h_ln}"
+
+        # "${_ehh}" "${_cr_f_}" "${fn_body_ln}" --vi0 ${name_fn_} --vr0 {name_fn_}
+    fi
+else
+echo 8
+    "${_ehh}" cp -rfv "${fn_dir_h_tml}" "${fn_dir_h_ln}"
+
+    # "${_ehh}" "${_cr_f_}" "${fn_body_ln}" --vi0 ${name_fn_} --vr0 {name_fn_}
+
+fi
+
+if ! "${_ehh}" ln -sv "${fn_dir_h_ln}" "${fn_dir_h_in}"; then
+    "${_plt_exit}" "ln -sv ${fn_dir_h_ln} ${fn_dir_h_in}"
+fi
+
