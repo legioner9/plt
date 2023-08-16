cc2779a765bcc001e91e6d7557273b748af9900c
[alt]legioner9 <legioner9@inbox.ru>
2023-08-16 19:07:24 +0700

    <> do el_f_prp_ #2

---
+++ actio/mall/dir_el_f_prp_/_gnr_dir_sys.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#. "${HOME}/.bashrc"
+
+filename="${PLT_PATH}/actio/mall/dir_el_f_prp_/_gnr_dir_sys.sh"
+
+ARGS=($@)
+NARGS=$#
+verbose=0
+[[ " ${ARGS[*]} " =~ " -verbose " ]] || verbose=1
+[[ 1 -eq ${verbose} ]] || echo -e "${HLIGHT}---$filename $* ---${NORMAL}" #started functions
+idir=$(pwd)
+rdir="$(prs_f -d $filename)"
+gname="$(prs_f -n $filename)" # name without .ext
+wrp_fifs1_ cd  "$(prs_f -d $filename)" -d1
+g_args=($(garg2e_ "${ARGS[@]}"))
+[[ 1 -eq ${verbose} ]] || echo -e "${GREEN}\${g_args[@]}: ${g_args[*]}${NORMAL}" #print variable
+for strex in $(garg2e_ "${ARGS[@]}"); do
+[[ 1 -eq ${verbose} ]] || echo "local $strex"
+eval $strex
+done
+_(){
+echo "empty _deb in $filename"
+# . $filename" arg1 arg2
+}
+if [ "_deb" == "$1" ]; then
+_
+return 0
+fi
+#{header}
+
+#----------------------------------------------------------------------
+#-------------------------------------
+#-------------------------------
+#? cr_tst_sys_ --name_dir extst_1 --ext extst"
+[ -z "$1" ] && {
+plt_exit "$1 empty first arg mast be name dir (like namefn_extst_num) in ${rdir}"
+return 1
+}
+#
+plt_pause "do that? create $1 (like namefn_extst_num) cr_tst_sys_ in ${rdir}"
+#
+${_ehh} rm ${rdir}/${gname}.tmp.exl
+#
+echo -e "
+${_ehh} cd ${rdir}
+#
+${_ehh} mkdir ${rdir}/"$1"_extst
+#
+${_ehh} cd ${rdir}/"$1"_extst
+#
+${_ehh} cr_tst_sys_ --name_dir extst_1 --ext extst" > ${rdir}/${gname}.tmp.exl
+#
+${_ehh}  ${_exl_} --_exl_list ${rdir}/${gname}.tmp.exl
+#
+${_ehh} rm ${rdir}/${gname}.tmp.exl
+#{body}
+#-------------------------------
+#-------------------------------------
+#----------------------------------------------------------------------
+
+cd "$idir"
+
+unset filename
--- actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/lg1_.nsc.sh
+++ actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/lg1_.nsc.sh
@@ -14,122 +14,121 @@ echo -e "${HLIGHT}--- start file: $filename with args: $@ ---${NORMAL}" # start 
 
 lg1_() {
 
-local dir_fns_="${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res"
+    local dir_fns_="${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res"
     #{def_const}
 
     #{ctrl_args}
 
-local ARGS=($@)
-local FNN=lg1_
-echo -e "${GREEN}\${ARGS[@]} = ${ARGS[*]}${NORMAL}" #print variable
-if [ -n "${ARGS[0]}" ] && { [ "${ARGS[0]}" == '-h' ] || { [ "${ARGS[0]:0:1}" != "-" ] && [ -n "$(grep -w "${ARGS[0]}" "${PLT_PATH}/.d/.nid/mall/mall.cntl")" ]; }; }; then
-for local_var_wrap in $(f2e "${PLT_PATH}/.d/.nid/mall/mall.local"); do
-# echo -e "${GREEN}$local_var_wrap = $local_var_wrap${NORMAL}" #print variable
-eval local "$local_var_wrap"
-local_var_wrap_arr+=("${local_var_wrap}")
-done
-if ! . "${PLT_PATH}/.d/.nid/mall/mall.const"; then
-${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall/mall.const"
-return 1
-fi
-if ! . "${PLT_PATH}/.d/.nid/mall/mall.nid"; then
-${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall/mall.nid"
-return 1
-fi
-echo -e "${HLIGHT}--- . ${PLT_PATH}/.d/.nid/mall/mall.nid, return 0 ---${NORMAL}" #start files
-return 0
-fi
-# if ! ${_garg2e2_} "${ARGS[@]}" 1>/dev/null; then
-if ! ${_garg2e2_} "${ARGS[@]}"; then
-${_plt_exit} " lg1_ return 1: ${FNLOCK}"
-return 1
-fi
-#? g_args with cntl=value
-local g_args=($(${_garg2e2_} "${ARGS[@]}"))
-#? e_args is {ARGS[@]} without cntl and cmtl_value - as it's free
-local e_args=($(${_earg2e2_} "${ARGS[@]}"))
-[[ 1 -eq ${verbose} ]] || echo -e "${GREEN}${g_args[@]}: ${g_args[*]}${NORMAL}" #print variable
-local strex
-for strex in $(${_garg2e2_} "${ARGS[@]}"); do
-[[ 1 -eq ${verbose} ]] || echo "local $strex"
-eval local $strex
-done
-local local_var_wrap
-local local_var_wrap_arr=()
-local local_var_fn
-local local_var_fn_arr=()
-for local_var_fn in $(f2e "${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.local.list"); do
-# echo -e "${GREEN}$local_var_fn = $local_var_fn${NORMAL}" #print variable
-eval local "$local_var_fn"
-local_var_fn_arr+=("${local_var_fn}")
-done
-for local_var_wrap in $(f2e "${PLT_PATH}/.d/.nid/mall/mall.local"); do
-# echo -e "${GREEN}$local_var_wrap = $local_var_wrap${NORMAL}" #print variable
-eval local "$local_var_wrap"
-local_var_wrap_arr+=("${local_var_wrap}")
-done
-# echo ". ${PLT_PATH}/.d/.nid/mall.nid"
-if ! . "${PLT_PATH}/.d/.nid/mall/mall.const"; then
-${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall/mall.const"
-return 1
-fi
-# echo ". ${FN_SAL_DIR}/lg1_.body.nsc.sh"
-if ! . "${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.nsc.sh"; then
-${_plt_exit} "fail: . ${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.sh"
-return 1
-fi
-cd "$PPWD" 1>/dev/null || plt_exit "fail : cd $PPWD"
+    local ARGS=($@)
+    local FNN=lg1_
+    echo -e "${GREEN}\${ARGS[@]} = ${ARGS[*]}${NORMAL}" #print variable
+    if [ -n "${ARGS[0]}" ] && { [ "${ARGS[0]}" == '-h' ] || { [ "${ARGS[0]:0:1}" != "-" ] && [ -n "$(grep -w "${ARGS[0]}" "${PLT_PATH}/.d/.nid/mall/mall.cntl")" ]; }; }; then
+        for local_var_wrap in $(f2e "${PLT_PATH}/.d/.nid/mall/mall.local"); do
+            # echo -e "${GREEN}$local_var_wrap = $local_var_wrap${NORMAL}" #print variable
+            eval local "$local_var_wrap"
+            local_var_wrap_arr+=("${local_var_wrap}")
+        done
+        if ! . "${PLT_PATH}/.d/.nid/mall/mall.const"; then
+            ${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall/mall.const"
+            return 1
+        fi
+        if ! . "${PLT_PATH}/.d/.nid/mall/mall.nid"; then
+            ${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall/mall.nid"
+            return 1
+        fi
+        return 0
+    fi
+    # if ! ${_garg2e2_} "${ARGS[@]}" 1>/dev/null; then
+    if ! ${_garg2e2_} "${ARGS[@]}"; then
+        ${_plt_exit} " lg1_ return 1: ${FNLOCK}"
+        return 1
+    fi
+    #? g_args with cntl=value
+    local g_args=($(${_garg2e2_} "${ARGS[@]}"))
+    #? e_args is {ARGS[@]} without cntl and cmtl_value - as it's free
+    local e_args=($(${_earg2e2_} "${ARGS[@]}"))
+    [[ 1 -eq ${verbose} ]] || echo -e "${GREEN}${g_args[@]}: ${g_args[*]}${NORMAL}" #print variable
+    local strex
+    for strex in $(${_garg2e2_} "${ARGS[@]}"); do
+        [[ 1 -eq ${verbose} ]] || echo "local $strex"
+        eval local $strex
+    done
+    local local_var_wrap
+    local local_var_wrap_arr=()
+    local local_var_fn
+    local local_var_fn_arr=()
+    for local_var_fn in $(f2e "${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.local.list"); do
+        # echo -e "${GREEN}$local_var_fn = $local_var_fn${NORMAL}" #print variable
+        eval local "$local_var_fn"
+        local_var_fn_arr+=("${local_var_fn}")
+    done
+    for local_var_wrap in $(f2e "${PLT_PATH}/.d/.nid/mall/mall.local"); do
+        # echo -e "${GREEN}$local_var_wrap = $local_var_wrap${NORMAL}" #print variable
+        eval local "$local_var_wrap"
+        local_var_wrap_arr+=("${local_var_wrap}")
+    done
+    # echo ". ${PLT_PATH}/.d/.nid/mall.nid"
+    if ! . "${PLT_PATH}/.d/.nid/mall/mall.const"; then
+        ${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall/mall.const"
+        return 1
+    fi
+    # echo ". ${FN_SAL_DIR}/lg1_.body.nsc.sh"
+    if ! . "${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.nsc.sh"; then
+        ${_plt_exit} "fail: . ${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.sh"
+        return 1
+    fi
+    cd "$PPWD" 1>/dev/null || plt_exit "fail : cd $PPWD"
     #{init_body}
 
 }
 
 lg1_c_() {
 
-local dir_fns_="${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res"
+    local dir_fns_="${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res"
     #{def_const}
 
     #{ctrl_args}
 
-local ARGS=($@)
-local FNN=lg1_
-# echo ". ${FN_SAL_DIR}/lg1_.body.nsc.sh"
-if ! . "${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.nsc.sh"; then
-${_plt_exit} "fail: . ${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.sh"
-return 1
-fi
+    local ARGS=($@)
+    local FNN=lg1_
+    # echo ". ${FN_SAL_DIR}/lg1_.body.nsc.sh"
+    if ! . "${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.nsc.sh"; then
+        ${_plt_exit} "fail: . ${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.sh"
+        return 1
+    fi
     #{init_body_c}
 
 }
 
 lg1_g_() {
 
-local dir_fns_="${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res"
+    local dir_fns_="${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res"
     #{def_const}
 
     #{ctrl_args}
 
-local ARGS=($@)
-local FNN=lg1_
-# if ! ${_garg2e2_} "${ARGS[@]}" 1>/dev/null; then
-if ! ${_garg2e2_} "${ARGS[@]}"; then
-${_plt_exit} " lg1_ return 1: ${FNLOCK}"
-return 1
-fi
-#? g_args with cntl=value
-local g_args=($(${_garg2e2_} "${ARGS[@]}"))
-#? e_args is {ARGS[@]} without cntl and cmtl_value - as it's free
-local e_args=($(${_earg2e2_} "${ARGS[@]}"))
-[[ 1 -eq ${verbose} ]] || echo -e "${GREEN}${g_args[@]}: ${g_args[*]}${NORMAL}" #print variable
-local strex
-for strex in $(${_garg2e2_} "${ARGS[@]}"); do
-[[ 1 -eq ${verbose} ]] || echo "local $strex"
-eval local $strex
-done
-# echo ". ${FN_SAL_DIR}/lg1_.body.nsc.sh"
-if ! . "${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.nsc.sh"; then
-${_plt_exit} "fail: . ${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.sh"
-return 1
-fi
+    local ARGS=($@)
+    local FNN=lg1_
+    # if ! ${_garg2e2_} "${ARGS[@]}" 1>/dev/null; then
+    if ! ${_garg2e2_} "${ARGS[@]}"; then
+        ${_plt_exit} " lg1_ return 1: ${FNLOCK}"
+        return 1
+    fi
+    #? g_args with cntl=value
+    local g_args=($(${_garg2e2_} "${ARGS[@]}"))
+    #? e_args is {ARGS[@]} without cntl and cmtl_value - as it's free
+    local e_args=($(${_earg2e2_} "${ARGS[@]}"))
+    [[ 1 -eq ${verbose} ]] || echo -e "${GREEN}${g_args[@]}: ${g_args[*]}${NORMAL}" #print variable
+    local strex
+    for strex in $(${_garg2e2_} "${ARGS[@]}"); do
+        [[ 1 -eq ${verbose} ]] || echo "local $strex"
+        eval local $strex
+    done
+    # echo ". ${FN_SAL_DIR}/lg1_.body.nsc.sh"
+    if ! . "${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.nsc.sh"; then
+        ${_plt_exit} "fail: . ${PLT_PATH}/actio/mall/dir_gig_f_/gig_f_extst/extst_1/ex1/_deb/res/dir_lg1_/.d.ax/.sal.ax/lg1_.body.sh"
+        return 1
+    fi
     #{init_body_g}
 
 }
