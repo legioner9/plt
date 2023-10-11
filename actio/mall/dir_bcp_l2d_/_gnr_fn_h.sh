ad4b745525ea290d2b80b49dbeb8249ac95e1fe7
[alt]legioner9 <legioner9@inbox.ru>
2023-10-11 13:26:53 +0700

    <> mall.nid #2

--- actio/mall/dir_bcp_l2d_/_gnr_fn_h.sh
+++ actio/mall/dir_bcp_l2d_/_gnr_fn_h.sh
@@ -12,27 +12,27 @@ verbose=0
 idir=$(pwd)
 rdir="$(prs_f -d $filename)"
 gname="$(prs_f -n $filename)" # name without .ext
-wrp_fifs1_ cd  "$(prs_f -d $filename)" -d1
+wrp_fifs1_ cd "$(prs_f -d $filename)" -d1
 g_args=($(garg2e_ "${ARGS[@]}"))
 [[ 1 -eq ${verbose} ]] || echo -e "${GREEN}\${g_args[@]}: ${g_args[*]}${NORMAL}" #print variable
 for strex in $(garg2e_ "${ARGS[@]}"); do
-[[ 1 -eq ${verbose} ]] || echo "local $strex"
-eval $strex
+    [[ 1 -eq ${verbose} ]] || echo "local $strex"
+    eval $strex
 done
-_(){
-echo "empty _deb in $filename"
-# . $filename" arg1 arg2
+_() {
+    echo "empty _deb in $filename"
+    # . $filename" arg1 arg2
 }
 if [ "_deb" == "$1" ]; then
-_
-return 0
+    _
+    return 0
 fi
 #{header}
 
 #----------------------------------------------------------------------
 #-------------------------------------
 #-------------------------------
-#? cr fn.h 
+#? cr fn.h
 ${_ehh} ${_el_f_h_} --_name_for_fn bcp_l2d_ --_dir_for_fns /home/st/REPOBARE/_repo/plt/actio/mall
 #{body}
 #-------------------------------
