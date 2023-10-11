c2ea42242024b9a91a6a06f1ac9aa203a595c041
[alt]legioner9 <legioner9@inbox.ru>
2023-10-11 17:30:56 +0700

    <> #3

--- .d/.exl/mall/cr_fn_h_file.sh
+++ .d/.exl/mall/cr_fn_h_file.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cr_f_dir_insert_ 
+cp -v "${h_tml}" "${hf_fn}"
+
+"${_cr_f_dir_insert_}" --name "${hf_fn}" --dir_insert "${FN_SAL_DIR}/${FNN}".d.h
+"${_cr_f_dir_insert_}" --name "${hf_fn}" --dir_insert "${PATH_NID_DIR}/mall/.insert"
+
+all_cntl="${PATH_NID_DIR}/mall/mall.cntl"
+"${_cr_f_}" "${hf_fn}" --fi0 "${all_cntl}" --fr0 "{all_cntl}"
