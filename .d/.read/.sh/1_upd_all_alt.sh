d1ae53194911977ae30cf4d69e18902640824a25
[alt]legioner9 <legioner9@inbox.ru>
2023-10-23 00:49:55 +0700

    <> ##

--- .d/.read/.sh/1_upd_all_alt.sh
+++ .d/.read/.sh/1_upd_all_alt.sh
@@ -2,6 +2,13 @@
 
 echo -e "${GREEN} start 1_upd_all_alt.sh ${NORMAL}" #print variable
 
+# apt-get update
+# apt-get dist-upgrade -d
+# apt-get dist-upgrade
+# apt-get clean
+
+sudo apt-get install eepm
+
 sudo epm full-upgrade --auto -y
 sudo epm autoremove -y
 sudo epm autoremove --direct
