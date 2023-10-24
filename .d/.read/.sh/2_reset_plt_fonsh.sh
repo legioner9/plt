9e728d56f1f02fe7bfa1dce54af5174435f9d939
[alt]legioner9 <legioner9@inbox.ru>
2023-10-24 15:35:34 +0700

    <> ##

--- .d/.read/.sh/2_reset_plt_fonsh.sh
+++ .d/.read/.sh/2_reset_plt_fonsh.sh
@@ -3,16 +3,26 @@
 echo -e "${GREEN} start 2_reset_plt_fonsh.sh ${NORMAL}" #print variable
 
 RESET_PLT_FONSH_TO_DEFAULT_=""
-read -p "START RESET_PLT_FONSH_TO_DEFAULT_ block? =================== y/n"
+read -p "START interactive RESET_PLT_FONSH_TO_DEFAULT_ block? =================== y/n"
 read RESET_PLT_FONSH_TO_DEFAULT_
 
 if [[ "${RESET_PLT_FONSH_TO_DEFAULT_}" == "y" ]]; then
 
-    rm -fv "$HOME"/.fonsh_path
-    rm -fv "$HOME"/.plt_path
-    rm -fv "$HOME"/.repo_path
-    rm -fv "$HOME"/.enterrc
-    rm -rfv "$HOME"/REPOBARE
-    rm -rfv "$HOME"/.ssh
+    rm -fvi "$HOME"/.fonsh_path
+    rm -fvi "$HOME"/.plt_path
+    rm -fvi "$HOME"/.repo_path
+    rm -fvi "$HOME"/.enterrc
+
+    read -p "DO? only 'y' remove all repo :: rm -rfv $HOME/REPOBARE"
+    read yes
+    if [[ "${yes:-0}" == "y" ]]; then
+        rm -rfv "$HOME"/REPOBARE
+    fi
+
+    read -p "DO? only 'y' remove .ssh :: rm -rfv $HOME/.ssh"
+    read yes
+    if [[ "${yes:-0}" == "y" ]]; then
+         rm -rfvi "$HOME"/.ssh
+    fi
 
 fi
