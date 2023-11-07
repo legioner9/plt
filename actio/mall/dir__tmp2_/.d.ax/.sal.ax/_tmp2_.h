6492f515db74ef7e493a9efeabacf35fef7366be
[alt]legioner9 <legioner9@inbox.ru>
2023-11-07 12:13:31 +0700

    <> cr _tmp2_ like _tmp_ #2
    gig_mall_v2_ _mdeb 4

---
+++ actio/mall/dir__tmp2_/.d.ax/.sal.ax/_tmp2_.h
@@ -0,0 +1,1 @@
+/home/st/REPOBARE/_repo/plt/.d/.sal.ax/_tmp2_/_tmp2_.h
--- actio/mall/dir__tmp_/_tmp_.nsc.sh
+++ actio/mall/dir__tmp_/_tmp_.nsc.sh
@@ -24,32 +24,32 @@ _tmp_() {
     local ARGS=($@)
     local NARGS=${#ARGS[@]}
     local FNN=_tmp_
-    local local_var_wrap
-    local local_var_wrap_arr=()
-    local local_var_fn
-    local local_var_fn_arr=()
+    local var_wrap
+    local var_wrap_arr=()
+    local var_fn
+    local var_fn_arr=()
 
     #* ------------
-    local local_arg_fn
-    local local_arg_fn_arr=()
-    local local_env_fn
-    local local_env_fn_arr=()
-    local local_def_pts_fn
-    local local_def_pts_fn_arr=()
-
+    local arg_fn
+    local arg_fn_arr=()
+    local env_fn
+    local env_fn_arr=()
+    local pts_fn
+    local pts_fn_arr=()
     #* ------------
 
     echo -e "${GREEN}\${ARGS[@]} = ${ARGS[*]}${NORMAL}" #print variable
     if [ -n "${ARGS[0]}" ] && { [ "${ARGS[0]}" == '-h' ] || { [ "${ARGS[0]:0:1}" != "-" ] && [ -n "$(grep -w "${ARGS[0]}" "${PLT_PATH}/.d/.nid/mall/mall.cntl")" ]; }; }; then
-        for local_var_wrap in $(f2e "${PLT_PATH}/.d/.nid/mall/mall.local"); do
-            # echo -e "${GREEN}$local_var_wrap = $local_var_wrap${NORMAL}" #print variable
-            eval local "$local_var_wrap"
-            local_var_wrap_arr+=("${local_var_wrap}")
+        for var_wrap in $(f2e "${PLT_PATH}/.d/.nid/mall/mall.local"); do
+            # echo -e "${GREEN}$var_wrap = $var_wrap${NORMAL}" #print variable
+            eval local "$var_wrap"
+            var_wrap_arr+=("${var_wrap}")
         done
         if ! . "${PLT_PATH}/.d/.nid/mall/mall.const"; then
             ${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall/mall.const"
             return 1
         fi
+        #? exec cntl nid wrapper
         if ! . "${PLT_PATH}/.d/.nid/mall/mall.nid"; then
             ${_plt_exit} "fail: . ${PLT_PATH}/.d/.nid/mall/mall.nid"
             return 1
@@ -67,46 +67,45 @@ _tmp_() {
     local g_args=($(${_garg2e2_} "${ARGS[@]}"))
     #? e_args is {ARGS[@]} without cntl and cmtl_value - as it's free
     local e_args=($(${_earg2e2_} "${ARGS[@]}"))
-    [[ 1 -eq ${verbose} ]] || echo -e "${GREEN}${g_args[@]}: ${g_args[*]}${NORMAL}" #print variable
+
     local strex
     for strex in $(${_garg2e2_} "${ARGS[@]}"); do
-        [[ 1 -eq ${verbose} ]] || echo "local $strex"
+
         eval local $strex
     done
     #! ----- END COMMENT IF NOT USED ${_garg2e2_} -----
-    for local_var_fn in $(f2e "${PLT_PATH}/actio/mall/dir__tmp_/.d.ax/.sal.ax/_tmp_.local.list"); do
-        # echo -e "${GREEN}$local_var_fn = $local_var_fn${NORMAL}" #print variable
-        eval local "$local_var_fn"
-        local_var_fn_arr+=("${local_var_fn}")
-    done
 
     #* -------------
+    #? mapping ${FNN}.local.list" file to var_fn_arr and local vars instead fn
+    for var_fn in $(f2e "${PLT_PATH}/actio/mall/dir__tmp_/.d.ax/.sal.ax/_tmp_.local.list"); do
+        # echo -e "${GREEN}$var_fn = $var_fn${NORMAL}" #print variable
+        eval local "$var_fn"
+        var_fn_arr+=("${var_fn}")
+    done
 
+    #? mapping obc_ file to env_fn_arr and local vars into fn as mutable internal values
     if [ -f "${obc_:-0}" ]; then
-        for local_arg_fn in $(f2e "${obc_}"); do
-            # echo -e "${GREEN}$local_var_fn = $local_var_fn${NORMAL}" #print variable
-            eval local "$local_arg_fn"
-            local_arg_fn_arr+=("${local_arg_fn}")
+        for arg_fn in $(f2e "${obc_}"); do
+            # echo -e "${GREEN}$var_fn = $var_fn${NORMAL}" #print variable
+            eval local "$arg_fn"
+            arg_fn_arr+=("${arg_fn}")
         done
     fi
 
+    #? mapping aer_ file to env_fn_arr and local vars into fn as immutable external values
     if [ -f "${aer_:-0}" ]; then
-        for local_env_fn in $(f2e "${obc_}"); do
-            # echo -e "${GREEN}$local_var_fn = $local_var_fn${NORMAL}" #print variable
-            eval local "$local_env_fn"
-            local_env_fn_arr+=("${local_env_fn}")
+        for env_fn in $(f2e "${aer_}"); do
+            # echo -e "${GREEN}$var_fn = $var_fn${NORMAL}" #print variable
+            eval local "$env_fn"
+            env_fn_arr+=("${env_fn}")
         done
     fi
 
+    #? visualisation pointers for use: mapping pts_ file to pts_fn_arr
     if [ -f "${pts_:-0}" ]; then
-        for local_def_pts_fn in $(f2e "${pts_}"); do
-            echo -e "${GREEN}\$local_def_pts_fn = $local_def_pts_fn${NORMAL}" #print variable
+        for pts_fn in $(f2e "${pts_}"); do
 
-            eval local val_"${local_def_pts_fn}"=\$"${local_def_pts_fn}"
-
-            eval local elem="${local_def_pts_fn}=\$${local_def_pts_fn}"
-
-            local_def_pts_fn_arr+=("${elem}")
+            pts_fn_arr+=("${pts_fn}")
 
         done
 
@@ -126,11 +125,11 @@ _tmp_() {
     # [sag] sagitta       - [набор файлов] стрела
 
     #* -------------
-
-    for local_var_wrap in $(f2e "${PLT_PATH}/.d/.nid/mall/mall.local"); do
-        # echo -e "${GREEN}$local_var_wrap = $local_var_wrap${NORMAL}" #print variable
-        eval local "$local_var_wrap"
-        local_var_wrap_arr+=("${local_var_wrap}")
+    #? mapping mall.local file to use in ${FNN}.body.nsc.sh
+    for var_wrap in $(f2e "${PLT_PATH}/.d/.nid/mall/mall.local"); do
+        # echo -e "${GREEN}$var_wrap = $var_wrap${NORMAL}" #print variable
+        eval local "$var_wrap"
+        var_wrap_arr+=("${var_wrap}")
     done
     # echo ". ${PLT_PATH}/.d/.nid/mall.nid"
     if ! . "${PLT_PATH}/.d/.nid/mall/mall.const"; then
