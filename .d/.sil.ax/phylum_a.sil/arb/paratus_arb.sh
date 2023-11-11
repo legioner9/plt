70d08f8f4baf6728235abd995ab9042ed8c0c97e
[alt]legioner9 <legioner9@inbox.ru>
2023-11-11 21:39:20 +0700

    <> paratus_arb.sh with ${arb_} #2

---
+++ .d/.sil.ax/phylum_a.sil/arb/paratus_arb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#?
+arb_837687944="${PLT_PATH}"/actio/mall/dir_fol_/.d.ax/.mdeb/.tmp/name_silva.sil/arb
+: >"${arb_837687944}"/all_ramus_name.lst
+for item_78467468 in $("${_dd2e}" "${arb_837687944}"); do
+    echo "$(prs_f -n "${item_78467468}")" >>"${arb_837687944}"/all_ramus_name.lst
+done
+
+: >${arb_837687944}/all_unique_tags.lst
+for item_83456832 in $(${_f2e} "${arb_837687944}"/all_ramus_name.lst); do
+
+    for item_98789 in $(${_f2e} ${arb_837687944}/${item_83456832}.ram/tags.fol); do
+        if ! grep -w ${item_98789} ${arb_837687944}/all_unique_tags.lst &>/dev/null; then
+            echo ${item_98789} >>${arb_837687944}/all_unique_tags.lst
+        fi
+    done
+
+done
+
+sort_tags_s ${arb_837687944}/all_unique_tags.lst
+sort_tags_s ${arb_837687944}/all_ramus_name.lst
