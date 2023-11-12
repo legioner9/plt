5a9568511864295031ba396b7f78a3449c3632f6
[alt]legioner9 <legioner9@inbox.ru>
2023-11-12 11:56:52 +0700

    <> do crram_ #2

--- actio/mall/dir_fol_/.d.ax/.mdeb/.tmp/name_silva.sil/arb/paratus_arb.sh
+++ actio/mall/dir_fol_/.d.ax/.mdeb/.tmp/name_silva.sil/arb/paratus_arb.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-#?
-arb_837687944="${PLT_PATH}"/actio/mall/dir_fol_/.d.ax/.mdeb/.tmp/name_silva.sil/arb
-: >"${arb_837687944}"/all_ramus_name.lst
-for item_78467468 in $("${_dd2e}" "${arb_837687944}"); do
-    echo "$(prs_f -n "${item_78467468}")" >>"${arb_837687944}"/all_ramus_name.lst
+#! inner procedure IFS for ${PLT_PATH}/.d/.sal.ax/fol_/fol_.sal
+#! local var {item_ram_} {item_tag_}
+#! {arb_} full path to arb dir with .ram files 
+
+: >"${arb_}"/all_ramus_name.lst
+for item_ram_ in $("${_dd2e}" "${arb_}"); do
+    echo "$(prs_f -n "${item_ram_}")" >>"${arb_}"/all_ramus_name.lst
 done
 
-: >${arb_837687944}/all_unique_tags.lst
-for item_83456832 in $(${_f2e} "${arb_837687944}"/all_ramus_name.lst); do
+: >${arb_}/all_unique_tags.lst
+for item_tag_1 in $(${_f2e} "${arb_}"/all_ramus_name.lst); do
 
-    for item_98789 in $(${_f2e} ${arb_837687944}/${item_83456832}.ram/tags.fol); do
-        if ! grep -w ${item_98789} ${arb_837687944}/all_unique_tags.lst &>/dev/null; then
-            echo ${item_98789} >>${arb_837687944}/all_unique_tags.lst
+    for item_tag_2 in $(${_f2e} ${arb_}/${item_tag_1}.ram/tags.fol); do
+        if ! grep -w ${item_tag_2} ${arb_}/all_unique_tags.lst &>/dev/null; then
+            echo ${item_tag_2} >>${arb_}/all_unique_tags.lst
         fi
     done
 
 done
 
-sort_tags_s ${arb_837687944}/all_unique_tags.lst
-sort_tags_s ${arb_837687944}/all_ramus_name.lst
+${_sort_tags_s} ${arb_}/all_unique_tags.lst
+${_sort_tags_s} ${arb_}/all_ramus_name.lst
--- actio/mall/dir_fol_/.d.ax/.mdeb/.tmp/name_silva.sil/arb/paratus_arb_v1.sh
+++ actio/mall/dir_fol_/.d.ax/.mdeb/.tmp/name_silva.sil/arb/paratus_arb_v1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #?
-arb_837687944="${arb_}"
+arb_837687944="${PLT_PATH}"/actio/mall/dir_fol_/.d.ax/.mdeb/.tmp/name_silva.sil/arb
 : >"${arb_837687944}"/all_ramus_name.lst
 for item_78467468 in $("${_dd2e}" "${arb_837687944}"); do
     echo "$(prs_f -n "${item_78467468}")" >>"${arb_837687944}"/all_ramus_name.lst
