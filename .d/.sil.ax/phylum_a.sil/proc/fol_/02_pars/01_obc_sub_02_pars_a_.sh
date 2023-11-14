51d1c07181eb7ccc264ab0255041b75db8423aa1
[alt]legioner9 <legioner9@inbox.ru>
2023-11-14 22:25:51 +0700

    <> do 01_obc_sub_02_pars_a_ #2

---
+++ .d/.sil.ax/phylum_a.sil/proc/fol_/02_pars/01_obc_sub_02_pars_a_.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#? return 2 => return 0, return 3 => return 1
+#{pre}
+
+01_obc_sub_02_pars_a_() {
+
+    echo -e "${CYAN}--- 01_obc_sub_02_pars_a_() file://{path_file} ---${NORMAL}" #sistem info mesage
+    echo -e "${CYAN}--- MAIN: ---${NORMAL}"                                      #sistem info mesage
+    #{body}
+
+}
--- .d/.sil.ax/phylum_a.sil/proc/fol_/02_pars/pars_obc_sub.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#? return 2 => return 0, return 3 => return 1
-
