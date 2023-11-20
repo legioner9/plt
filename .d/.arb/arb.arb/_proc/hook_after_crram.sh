d2390885dbbf77c7c5006921042edfac258de0dd
[alt]legioner9 <legioner9@inbox.ru>
2023-11-20 21:53:14 +0700

    <> arb #2

---
+++ .d/.arb/arb.arb/_proc/hook_after_crram.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# use: /home/st/REPOBARE/_repo/plt/.d/.arb/arb.arb /home/st/REPOBARE/_repo/plt/.d/.sil.ax/phylum_a.sil tag
---
+++ .d/.arb/tag.arb/_proc/hook_after_crram.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# use: /home/st/REPOBARE/_repo/plt/.d/.arb/tag.arb /home/st/REPOBARE/_repo/plt/.d/.sil.ax/phylum_a.sil pcg_mng
