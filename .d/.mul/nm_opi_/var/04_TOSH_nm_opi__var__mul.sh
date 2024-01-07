7476c18a1ed4f8682226324791e85350037dcf5c
[alt]legioner9 <legioner9@inbox.ru>
2024-01-07 14:46:27 +0700

    <> #2

---
+++ .d/.mul/nm_opi_/var/04_TOSH_nm_opi__var__mul.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#? return 2 => return 0, return 3 => return 1
+#{pre}
+
+04_TOSH_nm_opi__var__mul() {
+
+echo -e "${CYAN}--- 04_TOSH_nm_opi__var__mul() file://${PLT_PATH}/.d/.mul/nm_opi_/var/04_TOSH_nm_opi__var__mul.sh ---${NORMAL}" #sistem info mesage
+echo -e "${CYAN}--- MAIN: ---${NORMAL}" #sistem info mesage
+#{body}
+
+} 
