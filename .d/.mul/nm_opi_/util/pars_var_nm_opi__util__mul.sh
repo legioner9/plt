dbbebf3c053390ecaf0e5cef10a23375f4b0ed42
[alt]legioner9 <legioner9@inbox.ru>
2024-01-08 17:11:38 +0700

    <> add_remote_nm_opi__singl_fn__mul #2

--- .d/.mul/nm_opi_/util/pars_var_nm_opi__util__mul.sh
+++ .d/.mul/nm_opi_/util/pars_var_nm_opi__util__mul.sh
@@ -20,7 +20,7 @@ pars_var_nm_opi__util__mul() {
     # push_net
     # push_local
 
-    arr_all_ptr_var=(clone_net clone_local pull_net_strong pull_local_strong push_net_strong push_local_strong push_net push_local)
+    arr_all_item_repo=()
 
     if [ -n "${clone_net}" ] && [ -n "${clone_local}" ]; then
         plt_info "in pars_var_nm_opi__util__mul() : clone_ : EXIST '${clone_net}' && EXIST '${clone_local}' : return 1"
@@ -56,6 +56,7 @@ pars_var_nm_opi__util__mul() {
 
     if [ -n "${clone_local}" ]; then
         arr_clone_local=(${clone_local//<>/ })
+
         clone_local_addr=${arr_clone_local[0]}
 
         clone_abs_adr=${clone_local_addr}
@@ -67,5 +68,37 @@ pars_var_nm_opi__util__mul() {
             return 1
         fi
     fi
+
+    arr_all_ptr_var=(clone_net clone_local pull_net_strong pull_local_strong push_net_strong push_local_strong push_net push_local)
+
+    for item in ${arr_all_ptr_var[@]}; do
+
+        if [ -n "${!item}" ]; then
+            if [ -z "${str_all_repo[0]}" ]; then
+                str_all_repo="${!item}"
+            fi
+            str_all_repo="${str_all_repo}><${!item}"
+        fi
+
+    done
+
+    # echo -e "${GREEN}\$str_all_repo = $str_all_repo${NORMAL}" #print variable
+
+    arr_str_all_repo=(${str_all_repo//></ })
+
+    # parr3e_ arr_str_all_repo
+
+    str_all_unic_repo=
+    arr_str_all_unic_repo=()
+    for item in ${arr_str_all_repo[@]}; do
+
+        if ! [[ ${arr_str_all_unic_repo[*]} =~ ${item} ]]; then
+            arr_str_all_unic_repo+=("${item}")
+        fi
+    done
+
+    # echo -e "${GREEN}\${arr_str_all_unic_repo[@]} = ${arr_str_all_unic_repo[*]}${NORMAL}" #print variable
+
+    parr3e_ arr_str_all_unic_repo
 
 }
