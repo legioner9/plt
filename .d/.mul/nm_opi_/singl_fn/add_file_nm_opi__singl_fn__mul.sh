c09ab52a6ef7dcfe8cf59e38874c00371c345b30
[alt]legioner9 <legioner9@inbox.ru>
2024-01-11 03:57:44 +0700

    <> fix nothing to commit -> return 1 #2
    done fixsize in before_add in tgu

--- .d/.mul/nm_opi_/singl_fn/add_file_nm_opi__singl_fn__mul.sh
+++ .d/.mul/nm_opi_/singl_fn/add_file_nm_opi__singl_fn__mul.sh
@@ -8,6 +8,7 @@ add_file_nm_opi__singl_fn__mul() {
     echo -e "${CYAN}--- MAIN: ---${NORMAL}"                                                                                                          #sistem info mesage
     #{body}
 
+
     if [ -f "${REPO_PATH}/${name_repo}"/.d/.ham/ham/before_add_file_.ham ]; then
 
         echo -e "${BLUE}--- . "${REPO_PATH}/${name_repo}"/.d/.ham/ham/before_add_file_.ham ---${NORMAL}" #sistem info mesage
@@ -16,7 +17,17 @@ add_file_nm_opi__singl_fn__mul() {
             plt_exit "in nm_opi_() : FAIL_EXEC : '. file://${REPO_PATH}/${name_repo}/.d/.ham/ham/before_add_file_.ham' {with \$name_repo = '$name_repo'} : return 1"
             return 1
         fi
-        
+
+    fi
+
+    cd "${dir_repo}/${name_repo}" || {
+        plt_info "in add_file_nm_opi__singl_fn__mul() : FAIL_EXEC : 'cd file://${dir_repo}/${name_repo}' : return 1"
+        return 1
+    }
+
+    if ! git add .; then
+        plt_info "in add_file_nm_opi__singl_fn__mul() : FAIL_EXEC : 'git add .' : { pwd = ${dir_repo}/${name_repo} } : return 1"
+        return 1
     fi
 
 }
--- .d/.mul/nm_opi_/singl_fn/com_nm_opi__singl_fn__mul.sh
+++ .d/.mul/nm_opi_/singl_fn/com_nm_opi__singl_fn__mul.sh
@@ -8,4 +8,13 @@ echo -e "${CYAN}--- com_nm_opi__singl_fn__mul() file://${PLT_PATH}/.d/.mul/nm_op
 echo -e "${CYAN}--- MAIN: ---${NORMAL}" #sistem info mesage
 #{body}
 
+    cd "${dir_repo}/${name_repo}" || {
+        plt_info "in com_nm_opi__singl_fn__mul() : FAIL_EXEC : 'cd file://${dir_repo}/${name_repo}' : return 1"
+        return 1
+    }
+
+    if ! git commit; then
+        plt_info "in com_nm_opi__singl_fn__mul() : FAIL_EXEC : 'git commit' : { pwd = ${dir_repo}/${name_repo} } : return 1"
+    fi
+
 } 
