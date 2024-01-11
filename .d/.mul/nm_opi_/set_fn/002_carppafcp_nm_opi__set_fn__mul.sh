4c8bc244484dbe1511b5afd9ae9f9a3187b50f6b
[alt]legioner9 <legioner9@inbox.ru>
2024-01-11 15:13:40 +0700

    <> ##

--- .d/.mul/nm_opi_/set_fn/002_carppafcp_nm_opi__set_fn__mul.sh
+++ .d/.mul/nm_opi_/set_fn/002_carppafcp_nm_opi__set_fn__mul.sh
@@ -2,38 +2,38 @@
 #? return 2 => return 0, return 3 => return 1
 #{pre}
 
-carppafcp_nm_opi__set_fn__mul() {
+002_carppafcp_nm_opi__set_fn__mul() {
 
-    echo -e "${CYAN}--- carppafcp_nm_opi__set_fn__mul() file://${PLT_PATH}/.d/.mul/nm_opi_/set_fn/carppafcp_nm_opi__set_fn__mul.sh ---${NORMAL}" #sistem info mesage
+    echo -e "${CYAN}--- 002_carppafcp_nm_opi__set_fn__mul() file://${PLT_PATH}/.d/.mul/nm_opi_/set_fn/carppafcp_nm_opi__set_fn__mul.sh ---${NORMAL}" #sistem info mesage
     echo -e "${CYAN}--- MAIN: ---${NORMAL}"                                                                                                      #sistem info mesage
     #{body}
 
     # exec block start --------------------------------
 
     if ! clone_nm_opi__singl_fn__mul; then
-        plt_exit "in carp_nm_opi__set_fn__mul() : FAIL_EXEC : clone_nm_opi__singl_fn__mul {with \${name_repo}} = '${name_repo}'} : return 1"
+        plt_exit "in 002_carppafcp_nm_opi__set_fn__mul() : FAIL_EXEC : clone_nm_opi__singl_fn__mul {with \${name_repo}} = '${name_repo}'} : return 1"
         return 1
     fi
 
      #! check_ord
 
     if ! nm_opi_check_ord_name_repo; then
-        plt_exit "in carp_nm_opi__set_fn__mul() : FAIL_EXEC : nm_opi_check_ord_name_repo {with \${name_repo}} = '${name_repo}'} : return 1"
+        plt_exit "in 002_carppafcp_nm_opi__set_fn__mul() : FAIL_EXEC : nm_opi_check_ord_name_repo {with \${name_repo}} = '${name_repo}'} : return 1"
         return 1
     fi
 
     if ! add_remote_nm_opi__singl_fn__mul; then
-        plt_exit "in carp_nm_opi__set_fn__mul() : FAIL_EXEC : add_remote_nm_opi__singl_fn__mul {with \${name_repo}} = '${name_repo}'} : return 1"
+        plt_exit "in 002_carppafcp_nm_opi__set_fn__mul() : FAIL_EXEC : add_remote_nm_opi__singl_fn__mul {with \${name_repo}} = '${name_repo}'} : return 1"
         return 1
     fi
 
     if ! push_nm_opi__singl_fn__mul; then
-        plt_exit "in carp_nm_opi__set_fn__mul() : FAIL_EXEC : push_nm_opi__singl_fn__mul {with \${name_repo}} = '${name_repo}'} : return 1"
+        plt_exit "in 002_carppafcp_nm_opi__set_fn__mul() : FAIL_EXEC : push_nm_opi__singl_fn__mul {with \${name_repo}} = '${name_repo}'} : return 1"
         return 1
     fi
 
     if ! pull_nm_opi__singl_fn__mul; then
-        plt_exit "in carp_nm_opi__set_fn__mul() : FAIL_EXEC : pull_nm_opi__singl_fn__mul {with \${name_repo}} = '${name_repo}'} : return 1"
+        plt_exit "in 002_carppafcp_nm_opi__set_fn__mul() : FAIL_EXEC : pull_nm_opi__singl_fn__mul {with \${name_repo}} = '${name_repo}'} : return 1"
         return 1
     fi
 
--- .d/.mul/nm_opi_/singl_fn/com_nm_opi__singl_fn__mul.sh
+++ .d/.mul/nm_opi_/singl_fn/com_nm_opi__singl_fn__mul.sh
@@ -4,17 +4,21 @@
 
 com_nm_opi__singl_fn__mul() {
 
-echo -e "${CYAN}--- com_nm_opi__singl_fn__mul() file://${PLT_PATH}/.d/.mul/nm_opi_/singl_fn/com_nm_opi__singl_fn__mul.sh ---${NORMAL}" #sistem info mesage
-echo -e "${CYAN}--- MAIN: ---${NORMAL}" #sistem info mesage
-#{body}
+    echo -e "${CYAN}--- com_nm_opi__singl_fn__mul() file://${PLT_PATH}/.d/.mul/nm_opi_/singl_fn/com_nm_opi__singl_fn__mul.sh ---${NORMAL}" #sistem info mesage
+    echo -e "${CYAN}--- MAIN: ---${NORMAL}"                                                                                                #sistem info mesage
+    #{body}
 
     cd "${dir_repo}/${name_repo}" || {
         plt_info "in com_nm_opi__singl_fn__mul() : FAIL_EXEC : 'cd file://${dir_repo}/${name_repo}' : return 1"
         return 1
     }
 
-    if ! git commit; then
-        plt_info "in com_nm_opi__singl_fn__mul() : FAIL_EXEC : 'git commit' : { pwd = ${dir_repo}/${name_repo} } : return 1"
+    if [ "${nothing_commit}" -eq 0 ]; then
+
+        if ! git commit; then
+            plt_info "in com_nm_opi__singl_fn__mul() : FAIL_EXEC : 'git commit' : { pwd = ${dir_repo}/${name_repo} } : return 1"
+        fi
+
     fi
 
-} 
+}
