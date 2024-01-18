e4aa7f1b59a0330c70a9729e6c76cabe1ad4bf59
[alt]legioner9 <legioner9@inbox.ru>
2024-01-19 00:11:29 +0700

    <> ##

--- .d/.ham/main/main.fn.lst.d/exec_arb2tst_.sh
+++ .d/.ham/main/main.fn.lst.d/exec_arb2tst_.sh
@@ -3,7 +3,7 @@
 file_sh="${REPO_PATH}/${name_repo}"/.d/.ham/main/main.fn.lst.d/exec_arb2tst_.sh
 file_lst="${REPO_PATH}/${name_repo}"/.d/.ham/main/main.fn.lst.d/_exec_arb2tst_.lst
 
-echo -e "${BLUE}--- that file://${file_sh} ---${NORMAL}"               #sistem info mesage
+echo -e "${RED}--- that file://${file_sh} ---${NORMAL}"               #sistem info mesage
 echo -e "${HLIGHT}--- arb2tst_() wiwh file://${file_lst} ---${NORMAL}" #start files
 
 
@@ -13,7 +13,7 @@ if ! [ -f "${file_lst}" ]; then
     return 1
 fi
 
-if ! arb2tst_ "${file_lst}"; then
+if ! arb2tst_ "${file_lst}" tst; then
     plt_info "in file://${file_sh} : FAIL_EXEC : 'arb2tst_() wiwh file://${file_lst}' : return 1"
     return 1
 fi
