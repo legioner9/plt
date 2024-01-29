d9b4c17c1cdd257d5e7133cd644b06f0737849cb
[alt]legioner9 <legioner9@inbox.ru>
2024-01-29 19:57:36 +0700

    <> do punzip_to_ #2

--- actio/mall/dir_punzip_to_/punzip_to_tst/_flow_tst.sh
+++ actio/mall/dir_punzip_to_/punzip_to_tst/_flow_tst.sh
@@ -12,6 +12,32 @@ cd "${idir}" || {
 
 : >res
 
+dir_tst=${REPO_PATH}/plt/actio/mall/dir_punzip_to_/punzip_to_tst/dir_tst
+
+src=${dir_tst}/src.d/src.file.zip
+dist=${dir_tst}/dist.d
+
+# echo -e "${HLIGHT}--- rm ${dist} ---${NORMAL}" #start files
+# rm "${dist}"
+
+rm -v ${dir_tst}/dist.d/src.file
+# rm -v ${dist}
+
+echo -e "${HLIGHT}--- punzip_to_ ${PD_READ} file://${dist} file://${src} ---${NORMAL}" #start files
+punzip_to_ "${PD_READ}" "${dist}" "${src}"
+
+cd "${dist}" || plt_exit
+
+echo -e "${HLIGHT}--- unzip -P ${PD_READ} ${dist} ---${NORMAL}" #start files
+zip -P "${PD_READ}" src.file.zip src.file
+
+echo -e "${HLIGHT}--- diff ${dist}/src.file.zip ${src} ---${NORMAL}" #start files
+diff "${dist}"/src.file.zip "${src}"
+
+diff "${dist}"/src.file.zip "${src}" &>>"${idir}"/res
+
+echo >>"${idir}"/res
+
 # cr_tst_dir_ <dat &>>res
 
 #{body_sh}
---
+++ actio/mall/dir_punzip_to_/punzip_to_tst/dir_tst/_src.d/cr_zip_src.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dir_tst=${REPO_PATH}/plt/actio/mall/dir_punzip_to_/punzip_to_tst/dir_tst
+
+pzip_to_ ${PD_READ} ${dir_tst}/src.d/in_src.d.zip ${dir_tst}/src.d/in_src.d
+pzip_to_ ${PD_READ} ${dir_tst}/src.d/src.file.zip ${dir_tst}/src.d/src.file
