36b71bf26476a17684fd970fef4252e1818f7742
[alt]legioner9 <legioner9@inbox.ru>
2024-01-31 16:31:01 +0700

    <> ##

--- .d/.arb/osh.arb/man1_to_arb___.ram/.grot/man1_to_arb___.sh
+++ .d/.arb/osh.arb/man1_to_arb___.ram/.grot/man1_to_arb___.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #. "${HOME}/.bashrc"
-filename="${PLT_PATH}/.d/.arb/bsh.arb/t2c__.ram/.grot/t2c__.sh"
+filename="${PLT_PATH}/.d/.arb/osh.arb/man1_to_arb___.ram/.grot/man1_to_arb___.sh"
 echo -e "${HLIGHT}---start file://$filename ---${NORMAL}" # start file
 idir=$(pwd)
 # cd "$(prs_f -d $filename)" || qq_exit "$(prs_f -d $filename) not found"
 # garg_ $(prs_f -n $filename) $@ 1>/dev/null
 #{pre_fn}
 
-t2c__() {
+man1_to_arb___() {
     local FNN=${FUNCNAME[0]}
     local PPWD=$PWD
     local ARGS=($@)
@@ -15,17 +15,17 @@ t2c__() {
     local verbose=0
     [[ " ${ARGS[*]} " =~ " -verbose " ]] || verbose=1
     [[ 1 -eq ${verbose} ]] || echo -e "${CYAN}---$FNN() $* ---${NORMAL}" #started functions
-    local d_name=$(dirname ${PLT_PATH}/.d/.arb/bsh.arb/t2c__.ram/.grot/t2c__.sh)
+    local d_name=$(dirname ${PLT_PATH}/.d/.arb/osh.arb/man1_to_arb___.ram/.grot/man1_to_arb___.sh)
     # wrp_fifs1_ cd ${d_name} -d
     #{intro_fn}
     if [ "-h" == "$1" ]; then
         echo -e "${CYAN} ${FNN}() help: 
-MAIN: type nod as first char at ls -l
+MAIN: 
 NAME: ${FNN}()
 WHERE?:(only in root dir)Y/N
 WHAT?:(only abs path | only name file | any stile path )
 ARGS: 
-$1 nod_fs 
+$1
 [ ,$2 num_menu ]
 CNTLS:
 required
@@ -38,20 +38,7 @@ RETURN: ( result>stdout, return 0 | data | change to ptr |  fs_structure | ...)
 ERROR: ( (plt_err | plt_pause | plt_exit) errmes return 1 | ... )
 WARN: 
 DEBUG:
-EXAMP: 
-    - : file
-    d : dir
-    c : character devices
-    C : high performance (contiguous data) file
-    b : block devices
-    l : soft link (SYMBOLIC)
-    p : named pipe
-    s : socket
-    D : door (Solaris)
-    M : off-line (migrated) file (Cray DMF)
-    n : network special file (HP-UX)
-    P : port (Solaris)
-    ? : some other file type
+EXAMP:
 ${FNN} -<>
 ${NORMAL}"
         return 0
@@ -85,34 +72,6 @@ ${NORMAL}"
     # done
     #{default_cntl_fn}
     # amount_arg $# 1 1
-    if [ -z "$1" ]; then
-        plt_exit "in ${FNN} : first arg EMPTY : return 1"
-        return 1
-    fi
-
-    local ptr_path=$1
-    ptr_path=$(${_abs_path} $PPWD "ptr_path") #ptr args
-
-    if ! [ -e "$ptr_path" ]; then
-        plt_exit "in ${FNN} : NOT_EXIST : '$ptr_path' : return 1"
-        return 1
-    fi
-
-    if [ "$(file -b "$ptr_path")" == "directory" ]; then
-        echo d
-        return 0
-    fi
-
-    stat_str=$(ls -l ${ptr_path})
-    chr_stat_str=${stat_str:0:1}
-
-    if [ -n "${chr_stat_str}" ]; then
-        echo "${chr_stat_str}"
-        return 0
-    fi
-
-    plt_info "in ${FNN} : UNNOWN_ERROR : return 1"
-    return 1
     #{body_fn}
 }
 
---
+++ .d/.arb/osh.arb/man1_to_arb___.ram/.grot/man1_to_arb___tst/.d/.arb/man_1_tst.arb/_proc/hook_after_crram.sh.nl
@@ -0,0 +1,13 @@
+#!/bin/bash
+# use ${arb_} ${sil_} ${ram_} ${ram_to_create}
+
+#! global: ram_name_to_create
+
+# cd ${ram_to_create}/.grot || {
+#     plt_exit "EXEC_FAIL: cd file://${ram_to_create}/.grot return 3"
+#     return 3
+# }
+
+# if is_yes "touch 1.html"; then
+#     touch 1.html
+# fi
