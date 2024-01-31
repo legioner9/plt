02d1369d2e0c63d4a7f0eda772d038fdcb21fefa
[alt]legioner9 <legioner9@inbox.ru>
2024-01-31 19:38:34 +0700

    <> ##

---
+++ .d/.arb/osh.arb/pick_opennet___.ram/.grot/pick_opennet___extst/extst_1/ex2/_flow_tst.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+filename=${PLT_PATH}/.d/.arb/osh.arb/pick_opennet___.ram/.grot/pick_opennet___extst/extst_1/ex2/_flow_tst.sh
+# echo -e "${HLIGHT}---start file://$filename ---${NORMAL}" # start file
+PW=$(pwd)
+idir="$(dirname ${filename})"
+cd "${idir}" || {
+    echo "${idir} not dir" >&2
+    return 1
+}
+#?----------------------------------------------------
+#?-------------------------------------
+
+: >res
+
+# cr_tst_dir_ <dat &>>res
+
+#{body_sh}
+#?-------------------------------------
+#?----------------------------------------------------
+cd "$idir"
+unset filename
+unset PW
+unset idir
+#{ish}
