6abca3d764b4406241b95bec827b99dbf2ba1576
[alt]legioner9 <legioner9@inbox.ru>
2024-02-09 02:26:53 +0700

    <> ##

---
+++ .d/.arb/bsh.arb/RE__.ram/.grot/RE__extst/extst_1/ex1/_flow_tst.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+filename=${PLT_PATH}/.d/.arb/bsh.arb/RE__.ram/.grot/RE__extst/extst_1/ex1/_flow_tst.sh
+# echo -e "${HLIGHT}---start file://$filename ---${NORMAL}" # start file
+PW=$(pwd)
+idir="$(dirname ${filename})"
+cd "${idir}" || {
+    echo "${idir} not dir" >&2
+    return 1
+}
+#?----------------------------------------------------
+#?-------------------------------------
+
+: >res
+
+# cr_tst_dir_ <dat &>>res
+
+#{body_sh}
+#?-------------------------------------
+#?----------------------------------------------------
+cd "$idir"
+unset filename
+unset PW
+unset idir
+#{ish}
