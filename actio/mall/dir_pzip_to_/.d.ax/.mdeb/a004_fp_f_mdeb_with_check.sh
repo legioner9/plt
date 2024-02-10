3e4f915f3d466997903bee04cff2b70c3d2377b1
[alt]legioner9 <legioner9@inbox.ru>
2024-02-11 01:26:22 +0700

    <> see ##
    do . /home/st/REPOBARE/_repo/plt/actio/mall/dir_pzip_to_/.d.ax/.mdeb/a004_fp_f_mdeb_with_check.sh

--- actio/mall/dir_pzip_to_/.d.ax/.mdeb/a004_fp_f_mdeb_with_check.sh
+++ actio/mall/dir_pzip_to_/.d.ax/.mdeb/a004_fp_f_mdeb_with_check.sh
@@ -74,10 +74,83 @@ ${NORMAL}"
     # amount_arg $# 1 1
     #{body_fn}
 
-    cp -rf "${PLT_PATH}"/actio/mall/dir_pzip_to_/pzip_to_extst/extst_1/ex1/_003 "${PLT_PATH}"/actio/mall/dir_pzip_to_/.d.ax/.mdeb/.tmp/dist_4
+    # cp -rfv "${PLT_PATH}"/actio/mall/dir_pzip_to_/pzip_to_extst/extst_1/ex1/_003 "${PLT_PATH}"/actio/mall/dir_pzip_to_/.d.ax/.mdeb/.tmp/dist_4
 
-    
+    plt_info "START flow pzip_to_ \${PD_READ} \${dist} \${src} 1"
+
+    : >${dir_003}/res
+
+    dir_003="${PLT_PATH}"/actio/mall/dir_pzip_to_/.d.ax/.mdeb/.tmp/dist_4
+
+    # cp -rfv ${PLT_PATH}/actio/mall/dir_pzip_to_/pzip_to_extst/extst_1/ex1/_003 "${dir_003}"
+
+    #! FILE
+
+    src=${dir_003}/src.d/src.file
+    dist=${dir_003}/dist.d/dist.zip
+
+    # echo -e "${HLIGHT}--- rm ${dist} ---${NORMAL}" #start files
+    # rm "${dist}"
+
+    rm -v ${dir_003}/dist.d/src.file
+    # rm -v ${dist}
+
+    echo -e "${CYAN}--- pzip_to_ ${PD_READ} file://${dist} file://${src} 1 ---${NORMAL}" #start files
+    pzip_to_ "${PD_READ}" "${dist}" "${src}" 1
+
+    cd "${dir_003}"/dist.d || plt_exit
+
+    echo -e "${CYAN}--- unzip -P ${PD_READ} ${dist} ---${NORMAL}" #start files
+    unzip -P "${PD_READ}" "${dist}"
+
+    echo -e "${CYAN}--- cat file://${dir_003}/dist.d/src.file ---${NORMAL}" #start files
+    cat "${dir_003}"/dist.d/src.file
+    echo
+    cat "${dir_003}"/dist.d/src.file >>${dir_003}/res
+
+    echo >>${dir_003}/res
+
+    echo -e "${CYAN}--- diff ${dir_003}/dist.d/src.d ${dir_003}/src.d/src.d ---${NORMAL}" >>${dir_003}/res
+    diff "${dir_003}"/dist.d/src.file "${dir_003}"/src.d/src.file
+    diff "${dir_003}"/dist.d/src.file "${dir_003}"/src.d/src.file >>${dir_003}/res
+
+    #! DIR
+
+    src=${dir_003}/src.d/src.d
+    dist=${dir_003}/dist.d/dist_src_d.zip
+
+    rm -rv ${dir_003}/dist.d/src.d
+    # rm -v ${dist}
+
+    # echo -e "${HLIGHT}--- rm ${dist} ---${NORMAL}" #start files
+    # rm "${dist}"
+
+    echo -e "${CYAN}--- pzip_to_ ${PD_READ} file://${dist} file://${src} 1 ---${NORMAL}" #start files
+    pzip_to_ "${PD_READ}" "${dist}" "${src}" 1
+
+    cd "${dir_003}"/dist.d || plt_exit
+
+    echo -e "${CYAN}--- unzip -P ${PD_READ} -r ${dist} ---${NORMAL}" #start files
+    unzip -P "${PD_READ}" "${dist}"
+
+    echo -e "${CYAN}--- cat file://${dir_003}/dist.d/src.file ---${NORMAL}" #start files
+    cat "${dir_003}"/dist.d/src.d/in_src_d.file
+    echo
+    cat "${dir_003}"/dist.d/src.d/in_src_d.file >>${dir_003}/res
+
+    echo >>${dir_003}/res
+
+    echo -e "${CYAN}--- diff ${dir_003}/dist.d/src.d ${dir_003}/src.d/src.d ---${NORMAL}" >>${dir_003}/res
+    diff "${dir_003}"/dist.d/src.d "${dir_003}"/src.d/src.d >>${dir_003}/res
+
+    echo -e "${CYAN}--- cat "${dir_003}"/res ---${NORMAL}" #sistem info mesage
+    cat "${dir_003}"/res
+
+    pzip_to_ _extst
+
 }
+
+a004_fp_f_mdeb_with_check @
 
 cd "${idir}"
 unset filename
