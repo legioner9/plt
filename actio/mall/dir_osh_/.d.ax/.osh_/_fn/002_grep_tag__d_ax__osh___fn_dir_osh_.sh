65d4cf27bbf8209bc08dcad10dde2ad389a990c0
[alt]legioner9 <legioner9@inbox.ru>
2024-02-18 13:47:40 +0700

    <> do infn osh_ #2

---
+++ actio/mall/dir_osh_/.d.ax/.osh_/_fn/002_grep_tag__d_ax__osh___fn_dir_osh_.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#? return 2 => return 0, return 3 => return 1
+#{pre}
+
+002_grep_tag__d_ax__osh___fn_dir_osh_() {
+
+    echo -e "${CYAN}--- 002_grep_tag__d_ax__osh___fn_dir_osh_() file://${PLT_PATH}/actio/mall/dir_osh_/.d.ax/.osh_/_fn/002_grep_tag__d_ax__osh___fn_dir_osh_.sh ---${NORMAL}" #sistem info mesage
+    echo -e "${CYAN}--- MAIN: ---${NORMAL}"                                                                                                                                   #sistem info mesage
+    # local first_arg=
+    # first_arg=$1
+    # if [ -z "${first_arg}" ]; then
+    #     plt_info "002_grep_tag__d_ax__osh___fn_dir_osh_() : $1 : args NOT_DEFINE : return 1"
+    #     return 1
+    # fi
+    #! -------------- START check env -------------------
+    # if [ -z "${XXX}" ]; then
+    #     plt_info "in 002_grep_tag__d_ax__osh___fn_dir_osh_() : NOT_DEFINE : '${XXX}' : return 1"
+    #     return 1
+    # fi
+    #! -------------- END check env -------------------
+    #{body}
+
+}
