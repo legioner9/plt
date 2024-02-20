24a24a2868a5bd15173fb031c6af13c965da1df7
[alt]legioner9 <legioner9@inbox.ru>
2024-02-20 23:48:57 +0700

    <> garb_ -at -> mm tag -> mm ram #2

--- .d/.sil.ax/phylum_a.sil/proc/garb_/001_grep_name_proc_garb__phylum_a_sil.sh
+++ .d/.sil.ax/phylum_a.sil/proc/garb_/001_grep_name_proc_garb__phylum_a_sil.sh
@@ -4,20 +4,94 @@
 
 001_grep_name_proc_garb__phylum_a_sil() {
 
-echo -e "${CYAN}--- 001_grep_name_proc_garb__phylum_a_sil() file://${PLT_PATH}/.d/.sil.ax/phylum_a.sil/proc/garb_/001_grep_name_proc_garb__phylum_a_sil.sh ---${NORMAL}" #sistem info mesage
-echo -e "${CYAN}--- MAIN: ---${NORMAL}"                         #sistem info mesage
-# local first_arg=
-# first_arg=$1
-# if [ -z "${first_arg}" ]; then
-#     plt_info "001_grep_name_proc_garb__phylum_a_sil() : $1 : args NOT_DEFINE : return 1"
-#     return 1
-# fi
-#! -------------- START check env -------------------
-if [ -z "${XXX}" ]; then
-plt_info "in 001_grep_name_proc_garb__phylum_a_sil() : NOT_DEFINE : '\${XXX}' : return 1"
-return 1
-fi
-#! -------------- END check env -------------------
-#{body}
+    echo -e "${CYAN}--- 001_grep_name_proc_garb__phylum_a_sil() file://${PLT_PATH}/.d/.sil.ax/phylum_a.sil/proc/garb_/001_grep_name_proc_garb__phylum_a_sil.sh ---${NORMAL}" #sistem info mesage
+    echo -e "${CYAN}--- MAIN: ---${NORMAL}"                                                                                                                                  #sistem info mesage
+    # local first_arg=
+    # first_arg=$1
+    # if [ -z "${first_arg}" ]; then
+    #     plt_info "001_grep_name_proc_garb__phylum_a_sil() : $1 : args NOT_DEFINE : return 1"
+    #     return 1
+    # fi
+    #! -------------- START check env -------------------
+    if [ -z "${str_to_grep}" ]; then
+        plt_info "in 001_grep_name_proc_garb__phylum_a_sil() : NOT_DEFINE : '${str_to_grep}' : return 1"
+        return 1
+    fi
 
-} 
+    if [ -z "${arb_osh_dir}" ]; then
+        plt_info "in 001_grep_name_proc_garb__phylum_a_sil() : NOT_DEFINE : '${arb_osh_dir}' : return 1"
+        return 1
+    fi
+    #! -------------- END check env -------------------
+    #{body}
+    local mach_grep
+    local main_str
+    local tags_str
+    local arr_name_ram
+    local type_fol_to_grep
+
+    type_fol_to_grep="name"
+
+    arr_name_ram=()
+
+    for name_ram in $(d2e_ -n -dd ${arb_osh_dir} "ram "); do
+
+        # echo -e "${GREEN}\$name_ram = $name_ram${NORMAL}" #print variable
+        local path_to_ram=${arb_osh_dir}/${name_ram}.ram
+
+        mach_grep=$(echo ${name_ram} | grep "${str_to_grep}")
+
+        if [[ -n "${mach_grep}" ]]; then
+            echo -e "${GREEN}match : ${str_to_grep}${NORMAL}"         #print variable
+            echo -e "${RED}${type_fol_to_grep} : $mach_grep${NORMAL}" #print variable
+
+            if [ -f ${path_to_ram}/main.fol ]; then
+
+                main_str="$("${_f2oe}" "${path_to_ram}"/main.fol)"
+            else
+                plt_info "NOT_FILE 'file://${path_to_ram}/main.fol' continue"
+                continue
+            fi
+
+            if [ -f ${path_to_ram}/tags.fol ]; then
+                tags_str="$("${_f2str_}" "${path_to_ram}"/tags.fol)"
+            else
+                plt_info "NOT_FILE 'file://${path_to_ram}/tags.fol' continue"
+                continue
+            fi
+
+            echo -e "
+        ${BLUE}PATH: file://${path_to_ram}${NORMAL}
+        ${GREEN}NAME: ${name_ram}${NORMAL}
+        ${RED}MAIN: ${main_str}${NORMAL}
+        ${CYAN}TAGS: ${tags_str}${NORMAL}"
+
+            arr_name_ram+=("${name_ram}")
+
+        fi
+    done
+
+    if [[ -z "${arr_name_ram[*]}" ]]; then
+        plt_info "grep for tag at @${str_to_grep} EMPTY : return 0"
+        return 0
+    fi
+
+    arr_name_ram+=("exit")
+
+    PS3="eligendi actiones to exec or exit: "
+
+    select item_arr_name_ram in ${arr_name_ram[@]}; do
+
+        if [[ "${item_arr_name_ram}" == "exit" ]]; then
+            plt_info "return 0"
+            return 0
+        fi
+        echo -e "${HLIGHT}--- osh_ ${item_arr_name_ram} ---${NORMAL}" #start files
+        osh_ "${item_arr_name_ram}"
+        return 0
+    done
+    return 0
+    ------------- END check env -------------------
+    #{body}
+
+}
--- .d/.sil.ax/phylum_a.sil/proc/garb_/005_all_tag_proc_garb__phylum_a_sil.sh
+++ .d/.sil.ax/phylum_a.sil/proc/garb_/005_all_tag_proc_garb__phylum_a_sil.sh
@@ -4,20 +4,32 @@
 
 005_all_tag_proc_garb__phylum_a_sil() {
 
-echo -e "${CYAN}--- 005_all_tag_proc_garb__phylum_a_sil() file://${PLT_PATH}/.d/.sil.ax/phylum_a.sil/proc/garb_/005_all_tag_proc_garb__phylum_a_sil.sh ---${NORMAL}" #sistem info mesage
-echo -e "${CYAN}--- MAIN: ---${NORMAL}"                         #sistem info mesage
-# local first_arg=
-# first_arg=$1
-# if [ -z "${first_arg}" ]; then
-#     plt_info "005_all_tag_proc_garb__phylum_a_sil() : $1 : args NOT_DEFINE : return 1"
-#     return 1
-# fi
-#! -------------- START check env -------------------
-if [ -z "${XXX}" ]; then
-plt_info "in 005_all_tag_proc_garb__phylum_a_sil() : NOT_DEFINE : '\${XXX}' : return 1"
-return 1
-fi
-#! -------------- END check env -------------------
-#{body}
+    echo -e "${CYAN}--- 005_all_tag_proc_garb__phylum_a_sil() file://${PLT_PATH}/.d/.sil.ax/phylum_a.sil/proc/garb_/005_all_tag_proc_garb__phylum_a_sil.sh ---${NORMAL}" #sistem info mesage
+    echo -e "${CYAN}--- MAIN: ---${NORMAL}"                                                                                                                              #sistem info mesage
+    # local first_arg=
+    # first_arg=$1
+    # if [ -z "${first_arg}" ]; then
+    #     plt_info "005_all_tag_proc_garb__phylum_a_sil() : $1 : args NOT_DEFINE : return 1"
+    #     return 1
+    # fi
+    #! -------------- START check env -------------------
+    # if [ -z "${XXX}" ]; then
+    #     plt_info "in 005_all_tags__d_ax__osh___fn_dir_osh_() : NOT_DEFINE : '\${XXX}' : return 1"
+    #     return 1
+    # fi
+    #! -------------- END check env -------------------
+    #{body}
+    # -_utag
+    dir_sil=${SIL_A}
+    dir_arb=${PLT_PATH}/.d/.arb/osh.arb
 
-} 
+    echo -e "${HLIGHT}--- fol_ --_sil ${dir_sil} --_arb ${dir_arb} -_E ---${NORMAL}" #start files
+
+    fol_ \
+        --_sil "${dir_sil}" \
+        --_arb "${dir_arb}" \
+        -_utag
+
+    return 0
+
+}
