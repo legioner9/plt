a9d933a2b5f9486d372cd0129e21271606599c70
[alt]legioner9 <legioner9@inbox.ru>
2024-10-14 21:44:53 +0700

    <> ##

---
+++ .d/.install.ax/main_install_plt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# curl https://raw.githubusercontent.com/legioner9/plt/refs/heads/master/.d/.sh/main_install_stl.sh | sh
+# curl https://gitflic.ru/project/legioner9/plt/blob/raw?file=.d%2F.sh%2Fmain_install_stl.sh | sh
